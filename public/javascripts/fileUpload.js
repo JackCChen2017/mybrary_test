49879ceabcea2f040046e1f580019622f8d8cfba
JackCChen2017 <jackchenxing@live.com>
2020-07-26 19:19:51 +0800

    File Upload Database

---
+++ public/javascripts/fileUpload.js
@@ -0,0 +1,14 @@
+console.log("in fileUpload.js")
+FilePond.registerPlugin(
+    FilePondPluginImagePreview,
+    FilePondPluginImageResize,
+    FilePondPluginFileEncode,
+)
+
+FilePond.setOptions({
+    stylePanelAspectRatio: 150 / 100,
+    imageResizeTargetWidth: 100,
+    imageResizeTargetHeight: 150
+})
+
+FilePond.parse(document.body);
